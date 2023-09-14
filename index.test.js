3122dc6992a32e7d18bf290d783f1cd47b9a16d2
MrBisquit <william@wtdawson.info>
2023-09-14 21:05:03 +0100

    More tests

--- index.test.js
+++ index.test.js
@@ -1,4 +1,5 @@
 const index = require("./index");
+const threatCheck = require("./lib/threatCheck");
 
 test('Test the checkExists function.', () => {
     expect(index.checkExists("Hello")).toBe(true);
@@ -7,4 +8,37 @@ test('Test the checkExists function.', () => {
     expect(index.checkExists("")).toBe(false);
     expect(index.checkExists(null)).toBe(false);
     expect(index.checkExists(undefined)).toBe(false);
+});
+
+test('Test threat checking', () => {
+    const data = [
+        {
+            data : {
+                request : {
+                    url : "/"
+                }
+            },
+            expect : {
+                threat : false,
+                ctl : 0,
+                threats : []
+            }
+        },
+        {
+            data : {
+                request : {
+                    url : "/SELECT"
+                }
+            },
+            expect : {
+                threat : false,
+                ctl : 0.05,
+                threats : [{ item : "SELECT", level : 0.5 }]
+            }
+        }
+    ]
+
+    data.forEach(item => {
+        expect(threatCheck(item)).toBe(item.expect);
+    });
 });
